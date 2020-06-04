dcd09fc2ef9dc1025527bacdc265f50e5bc199fa
Eduardo <egbulsoni@gmail.com>
2020-06-04 00:45:46 -0300

    fixing Percolation corner cases (index between 1 and n)

---
+++ Percolation.java
@@ -0,0 +1,137 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    private boolean[][] grid;
+    //    private boolean percolated = false;
+    private int openSites = 0;
+    private final WeightedQuickUnionUF gridid;
+    private final int sink;
+
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException();
+        gridid = new WeightedQuickUnionUF(n * n + 2);
+        grid = new boolean[n + 1][n + 1];
+        sink = getGridId(n, n) + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = false;
+            }
+        }
+
+    }
+
+    private int getGridId(int row, int col) {
+        return (row - 1) * grid.length + col;
+    }
+
+
+    private void uniteNeighbors(int row, int col) {
+
+        int neighborRow;
+        int neighborCol;
+        int currentPoint = getGridId(row, col);
+
+        neighborRow = row - 1;
+        neighborCol = col;
+        // check top
+        if (neighborRow >= 0) {
+            if (grid[neighborRow][neighborCol])
+                gridid.union(currentPoint, getGridId(neighborRow, neighborCol));
+
+        } else {
+            // default
+
+            gridid.union(0, currentPoint);
+        }
+        // check bottom
+        neighborRow = row + 1;
+        if (neighborRow < grid.length) {
+            if (grid[neighborRow][neighborCol])
+                gridid.union(currentPoint, getGridId(neighborRow, neighborCol));
+        } else if (!percolates()) {
+            // default
+            gridid.union(sink, currentPoint);
+        }
+
+        // check right
+        neighborRow = row;
+        neighborCol = col + 1;
+        if (neighborCol < grid.length)
+            if (grid[neighborRow][neighborCol])
+                gridid.union(currentPoint, getGridId(neighborRow, neighborCol));
+
+
+        // check left
+        neighborCol = col - 1;
+        if (neighborCol >= 0)
+            if (grid[neighborRow][neighborCol])
+                gridid.union(currentPoint, getGridId(neighborRow, neighborCol));
+
+
+    }
+
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        try {
+            if ((row >= 0 && row < grid.length) && (col >= 0 && col < grid.length))
+                if (!grid[row][col]) {
+                    grid[row][col] = true;
+                    openSites += 1;
+                    uniteNeighbors(row, col);
+                    StdOut.println("row = " + row + " col = " + col);
+
+                }
+        } catch (IllegalArgumentException ex) {
+//            StdOut.println(ex.getMessage());
+        }
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        try {
+            return grid[row][col];
+        } catch (IllegalArgumentException ex) {
+//            StdOut.println(ex.getMessage());
+
+        }
+
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        try {
+            return gridid.find(0) == gridid.find(getGridId(row, col));
+        } catch (IllegalArgumentException ex) {
+//            StdOut.println(ex.getMessage());
+        }
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return gridid.find(0) == gridid.find(sink);
+    }
+
+
+//    public static void main(String[] args) {
+//
+//        Percolation perc = new Percolation(14);
+//        perc.open(14, 0);
+//        perc.open(-1, -1);
+//        perc.open(3, 3);
+//        perc.open(4, 3);
+//        perc.open(4, 6);
+//
+//    }
+}
---
+++ PercolationStats.java
@@ -0,0 +1,85 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+
+public class PercolationStats {
+
+    private static final double AMOUNT = 1.96;
+    private final double[] thresholds;
+    private final int trials;
+
+
+    //    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1) {
+            throw new IllegalArgumentException("arguments must be greater than 1");
+        }
+
+        this.trials = trials;
+
+        thresholds = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            int opened = 0;
+            while (!perc.percolates()) {
+                int p = StdRandom.uniform(0, n);
+                int q = StdRandom.uniform(0, n);
+                if (!perc.isOpen(p, q)) {
+                    perc.open(p, q);
+                    opened++;
+                }
+
+
+            }
+            double s = (double) opened / (double) n * n;
+//            StdOut.println(s);
+            thresholds[i] = s;
+        }
+
+
+    }
+
+    //    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+
+    //    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
+
+    //
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - (AMOUNT * stddev()) / Math.sqrt(this.trials);
+    }
+
+    //
+//    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + (AMOUNT * stddev()) / Math.sqrt(this.trials);
+    }
+
+    //
+//    // test client (see below)
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, t);
+        StdOut.println("mean = " + stats.mean());
+        StdOut.println("std deviation = " + stats.stddev());
+        StdOut.println("95% confidence hi = " + stats.confidenceHi());
+        StdOut.println("95% confidence lo = " + stats.confidenceLo());
+
+
+//        System.out.println("Type the size of the grid: ");
+//        System.out.println("How many trials? ");
+//        PercolationStats ps = new PercolationStats(gridSize, trials);
+
+
+    }
+}
